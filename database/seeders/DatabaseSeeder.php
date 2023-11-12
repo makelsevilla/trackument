2685180fe81bcf446ace83131f2cf8773184c8be
Michael Sevilla <makelsevilla@gmail.com>
2023-11-12 17:46:32 +0800

    modified the database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::create([
+            "name" => "System Administrator",
+            "username" => "admin",
+            "password" =>
+                '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            "role" => "admin",
+        ]);
+
+        \App\Models\User::create([
             "name" => "MIS Office",
             "username" => "mis_office",
             "password" =>
                 '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            "role" => "admin",
+            "role" => "user",
         ]);
 
         \App\Models\User::create([
